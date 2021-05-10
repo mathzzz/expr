93b9d7e8abfe03104dea86f55c808e66b9b281ee
zhuangjuwei <zhuangjuwei@163.com>
2021-05-11 00:42:38 +0800

    support comma expr

--- expr.c
+++ expr.c
@@ -4,25 +4,16 @@
 #include <string.h>
 
 int expr(int oper);
-char math[100]; //"-1+2*3/-2--3-1+2+3+4*5/-2-6+7-8*2/-4-5-7+++++-9";
+int get_op(void);
+void unget(void);
+
+char math[100]; 
 char *pc;
- 
-int get_pri(int op)
-{
-	switch (op) {
-	case '+':
-	case '-':
-		return 1;
-	case '*':
-	case '/':
-		return 2;
-	}
-	exit(-1);
-}
+int reg[26];
 
 int pri(int op1, int op2)
 {
-	char map[] = { [0]=0, ['(']=1, [')']=1, ['+']=2, ['-']=2, ['*']=3, ['/']=3};
+	char map[] = { [0]=0, ['='] = 1, [','] = 1, [';']=2, ['+']=3, ['-']=3, ['*']=4, ['/']=4};
 	int pri1 = map[op1];
 	int pri2 = map[op2];
 	return pri1 - pri2;
@@ -35,8 +26,17 @@ int get_num(void)
 		return expr(ch);
 	else if(ch == '-')
 		return -expr(ch);
-	else if(ch == '(' )
+	else if(ch == '(' || ch == '{')
 		return expr(ch);
+	else if(ch >= 'a' && ch <= 'z') {
+		int op = get_op();
+		if(op == '=') { // left value
+			return reg[ch - 'a'] = expr(op);
+		}else { // right value
+			unget();
+			return reg[ch - 'a'];
+		}
+	}
 	return ch - '0';
 }
 
@@ -58,32 +58,40 @@ int peek(void)
 
 int expr(int oper)
 {
-	int num;
-	int op; 
-
+	int num, op, t;
 	num = get_num();	
 
 loop:
 	op = get_op();
+	if(oper == '(' && op == ')')
+		return num;
+	if(oper == '{' && op == '}')
+		return num;
+
 	if(pri(oper,op)>= 0) {
-		if(!(oper == '(' && op == ')'))
-			unget();
+		unget();
 		return num;
 	}
 
 	//  oper < op
 	switch(op) {
+	case ',':
+		num = expr(op);
+		break;
 	case '+':
-			num = num + expr(op);
+		num = num + (t=expr(op));
 		break;
 	case '-':
-			num = num - expr(op);
+		num = num - expr(op);
 		break;
 	case '*':
 		num = num * expr(op);
 		break;
 	case '/':
 		num = num / expr(op);
+		break;
+	case '%':
+		num  = num % expr(op);
 		break;
 	}
 
@@ -92,18 +100,19 @@ loop:
 
 int main(int argc, char **argv)
 {
+	int i;
 	strcpy(math, argv[1]);
 	pc = math;
-
 	int ans = expr('\0');
-
-	printf("ans=%d\n", ans);
-
+	for(i=0; i<5; i++){
+		printf("%c=%d,", 'a'+i, reg[i]);
+	}
+	printf("|%s:ans=%d\n", math, ans);
 	char buf[120];
 	
 	sprintf(buf, "echo $((%s))", math);
 
-	system(buf);
+	//system(buf);
 
 	return 0;
 }
